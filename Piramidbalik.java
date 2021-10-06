65ac44fd2302be5601cf86bcaf13552a7d6f7896
Daffa Daris Mahendra Ansori <80310814+AxXxolotl-boop@users.noreply.github.com>
2021-10-06 10:11:40 +0700

    Add files via upload

---
+++ Piramidbalik.java
@@ -0,0 +1,27 @@
+
+class Piramidbalik{
+
+    public static void main(String args[])
+    {
+        int n = 3;
+
+        printPattern(n);
+    }
+
+    static void printPattern(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++)
+            {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+    }
+}
+
